3c32cec20280f139832890b85b5c5548f0e8a610
Michael Voke <gadocer54@gmail.com>
2023-01-15 21:44:29 +0100

    modified:   image_class.py

--- image_class.py
+++ image_class.py
@@ -1,6 +1,20 @@
 import tensorflow as tf
 import numpy as np
 
-from tensorflow import keras
+import keras
 
 import matplotlib.pyplot as plt
+
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+print(train_labels[5600])
+
+plt.imshow(train_images[5600], cmap='gray', vmin=0, vmax=255)
+
+plt.show()
+
+
