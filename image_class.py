86d2b01ea4cfa7df8ac7d541f21f14ebbd2256db
Michael Voke <gadocer54@gmail.com>
2023-01-16 02:46:25 +0100

    new file:   image_class.h5 	modified:   image_class.py

--- image_class.py
+++ image_class.py
@@ -13,12 +13,13 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 print(train_labels[1])
 
-plt.imshow(train_images[1], cmap='gray', vmin=0, vmax=255)
+# plt.imshow(train_images[1], cmap='gray', vmin=0, vmax=255)
 
-plt.show()
+# plt.show()
 
 
 model = keras.Sequential([
+    
     keras.layers.Flatten(input_shape=(28, 28)),
     
     keras.layers.Dense(units=128, activation=tf.nn.relu),
@@ -26,6 +27,23 @@ model = keras.Sequential([
     keras.layers.Dense(units=10, activation =tf.nn.softmax)
 ])
 
-model.compile(optimizer=tf.optimizers.Adam(), loss='sparse_categorical_crossentropy')
+
+model.compile(optimizer=tf.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 model.fit(train_images, train_labels, epochs=5)
+
+# model.save('image_class.h5')
+
+avg_loss = model.evaluate(test_images, test_labels)
+
+prediction = model.predict(test_images)
+
+print(test_labels[10])
+
+plt.imshow(train_images[10], cmap='gray', vmin=0, vmax=255)
+
+plt.show()
+
+print(prediction[10])
+
+print(list(prediction[10]).index(max(prediction[10])))
